5517c99adc6e3fb9e1b0445afb36a916693afc18
ATIL YALI <atilihsan38@gmail.com>
2024-05-13 22:54:49 +0300

    new hope

--- app.mjs
+++ app.mjs
@@ -9,7 +9,7 @@ import FileSync from "lowdb/adapters/FileSync.js";
 import { spawn } from "child_process";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
