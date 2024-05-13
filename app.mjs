378b4b9f440b7bf920be0aa5d6520d2da9df6240
ATIL YALI <atilihsan38@gmail.com>
2024-05-13 22:55:05 +0300

    port 8080 to 3000

--- app.mjs
+++ app.mjs
@@ -9,7 +9,7 @@ import FileSync from "lowdb/adapters/FileSync.js";
 import { spawn } from "child_process";
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
